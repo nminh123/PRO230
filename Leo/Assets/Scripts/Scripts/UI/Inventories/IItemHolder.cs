1a6ba9ea1ecdda4540ee1c98ea204e0c9e2f38c5
Minh280123 <127473811+Minh280123@users.noreply.github.com>
2025-03-18 13:26:39 +0700

    update inventory

---
+++ Leo/Assets/Scripts/Scripts/UI/Inventories/IItemHolder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Minh.Inventories;
+
+namespace Minh.UI.Inventories
+{
+    /// <summary>
+    /// Allows the `ItemTooltipSpawner` to display the right information.
+    /// </summary>
+    public interface IItemHolder
+    {
+        InventoryItem GetItem();
+    }
+}

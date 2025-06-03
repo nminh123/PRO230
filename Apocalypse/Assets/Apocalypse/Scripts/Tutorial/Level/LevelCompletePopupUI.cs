04c22357e873f8b8a7b40f0798633d61aa0627b4
Fjnnn2907 <phuongphipho2005@gmail.com>
2025-06-03 17:17:48 +0700

    new update : them tinh nang vuot ai nhan thuong

---
+++ Apocalypse/Assets/Apocalypse/Scripts/Tutorial/Level/LevelCompletePopupUI.cs
@@ -0,0 +1,69 @@
+﻿using Game.Tutorial;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCompletePopupUI : Singleton<LevelCompletePopupUI>
+{
+    [Header("UI References")]
+    public GameObject popupPanel;
+    public GameObject[] starIcons;
+
+    public ItemRewardByLevelPopup[] rewardSlots;
+
+    public Button homeButton;
+    public Button replayButton;
+    public Button nextButton;
+
+    private void Start()
+    {
+        homeButton.onClick.AddListener(OnClickHome);
+        replayButton.onClick.AddListener(OnClickReplay);
+        nextButton.onClick.AddListener(OnClickNext);
+    }
+
+    public void Show(int starsEarned, (ItemSO item, int quantity)[] rewards)
+    {
+        popupPanel.SetActive(true);
+
+        for (int i = 0; i < starIcons.Length; i++)
+        {
+            starIcons[i].SetActive(i < starsEarned);
+        }
+
+
+        foreach (var slot in rewardSlots)
+        {
+            slot.rewardImage.transform.parent.gameObject.SetActive(false);
+        }
+
+        for (int i = 0; i < rewards.Length && i < rewardSlots.Length; i++)
+        {
+            rewardSlots[i].rewardImage.sprite = rewards[i].item.image;
+            rewardSlots[i].rewardText.text = rewards[i].quantity.ToString();
+
+            rewardSlots[i].rewardImage.transform.parent.gameObject.SetActive(true);
+        }
+    }
+
+    public void Hide()
+    {
+        popupPanel.SetActive(false);
+    }
+
+    public void OnClickHome()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Map");
+    }
+
+    public void OnClickReplay()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void OnClickNext()
+    {
+        //LevelManager.SaveLevelProgress(LevelManager.currentLevel, starsEarned: 3); // Có thể nhận vào nếu cần
+        //LevelManager.currentLevel++;
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Map");
+    }
+}
---
+++ Apocalypse/Assets/Apocalypse/Scripts/Tutorial/Level/TestLevel.cs
@@ -0,0 +1,21 @@
+﻿using Game.Tutorial;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestLevel : MonoBehaviour
+{
+    [SerializeField] private ItemRewardByLevel[] itemRewards;
+    public void OnLevelComplete(int starsEarned)
+    {
+        var rewards = new (ItemSO, int)[itemRewards.Length];
+        for (int i = 0; i < itemRewards.Length; i++)
+        {
+            rewards[i] = (itemRewards[i].ItemSO, itemRewards[i].rewardAmounts);
+        }
+
+        LevelCompletePopupUI.Instance.Show(starsEarned, rewards);
+
+        LevelManager.Instance.SaveLevelProgress(starsEarned);
+        //SceneManager.LoadScene("Map"); 
+    }
+}
